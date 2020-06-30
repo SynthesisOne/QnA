d16feb2e02d749ac07c42ee340ae5058ac127705
SynthesisOne <developerfromrussia@gmail.com>
2020-06-30 20:32:13 +0300

    fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should have_many(:answers) }
   it { should belong_to(:user) }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
   it { should  have_db_column(:title) }
   it { should  have_db_column(:body) }
 end
