00902f3188e6332dfba013507abef33f9ae4cb14
SynthesisOne <developerfromrussia@gmail.com>
2020-07-19 02:10:13 +0300

    polymoprphic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,9 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-  def show; end
+  def show
+    answer.links.new # build создаем связанный объект
+  end
 
   def new
     question
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,10 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { is_expected.to have_many(:links).dependent(:destroy) }
+
   it { is_expected.to belong_to(:question) }
   it { is_expected.to belong_to(:user) }
 
   it { is_expected.to validate_presence_of :body }
+
+  it { should accept_nested_attributes_for :links }
 
   it { is_expected.to  have_db_column(:question_id) }
   it { is_expected.to  have_db_column(:body) }
