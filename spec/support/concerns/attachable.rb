4a600dac14210b63d299818209b9c11eafa59aae
SynthesisOne <developerfromrussia@gmail.com>
2020-07-23 21:37:37 +0300

    add concerns

---
+++ spec/support/concerns/attachable.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+shared_examples_for 'attachable' do
+  it 'have many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
+end
