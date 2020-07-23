b3d3762d8e8c04aa25e1fdd46b97145885bc794f
SynthesisOne <developerfromrussia@gmail.com>
2020-07-23 19:59:19 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  include Gachibass
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
