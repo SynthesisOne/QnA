219eef2102624d681c7148bd1fa7d0f47edeff55
SynthesisOne <developerfromrussia@gmail.com>
2020-07-27 13:17:27 +0300

    after rewiew

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -5,11 +5,11 @@ module Voted
     before_action :set_votable, only: %i[positive_vote negative_vote] # except is the opposite: only
 
     def positive_vote
-      user_vote <= 0 ? vote(1) : cancel_vote
+      @votable.user_vote(current_user) <= 0 ? vote(1) : cancel_vote
       end
 
     def negative_vote
-      user_vote >= 0 ? vote(-1) : cancel_vote
+      @votable.user_vote(current_user) >= 0 ? vote(-1) : cancel_vote
     end
 
     private
@@ -24,28 +24,21 @@ module Voted
 
     def vote(number)
       if current_user.author_of?(@votable)
+
         render json: { error: 'You cannot vote for yourself' }, status: 422
       else
-        user_voted.any? ? user_voted.first.update(score: number) : @votable.votes.create(score: number, user: current_user)
+        @votable.formation_vote(current_user, number)
         render json: { id: @votable.id, type: votable_name(@votable), rating: @votable.rating, message: 'You have successfully voted' }
-    end
-    end
-
-    def user_vote
-      user_voted.sum(:score)
+      end
     end
 
     def votable_name(obj)
       obj.class.name.downcase
     end
 
-    def user_voted
-      @votable.votes.where(user_id: current_user.id)
-    end
-
     def cancel_vote
-      user_voted.destroy_all
-      render json: { id: @votable.id, type: votable_name(@votable), rating: @votable.rating, message: 'You canceled your vote' }
+      @votable.user_voted(current_user).destroy_all
+        render json: { id: @votable.id, type: votable_name(@votable), rating: @votable.rating, message: 'You canceled your vote' }
     end
   end
 end
