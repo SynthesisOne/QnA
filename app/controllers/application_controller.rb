b4f08f28ef6575f35afe1a5d22bf079fb09e8306
SynthesisOne <developfromrussia@gmail.com>
2020-08-21 22:43:15 +0300

    after rewiew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,20 @@ class ApplicationController < ActionController::Base
   before_action :gon_user
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
+    respond_to do |format|
+      format.html do
+        redirect_to root_path, alert: exception.message
+      end
+      format.json do
+        render json: { error: exception.message }, status: 422
+      end
+      format.js do
+        render json: { error: exception.message }, status: 422
+      end
+    end
   end
+
+  check_authorization unless: :devise_controller?
 
   private
 
