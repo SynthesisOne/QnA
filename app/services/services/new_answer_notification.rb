13ad6100b4baa18dfe097ba1d0e29dede23b5c72
SynthesisOne <developfromrussia@gmail.com>
2020-09-05 15:41:47 +0300

    after rewiew fix

--- app/services/services/new_answer_notification.rb
+++ app/services/services/new_answer_notification.rb
@@ -1,10 +1,14 @@
 class Services::NewAnswerNotification
   def send_notification(answer)
     answer.question.subscribers.find_each(batch_size: 500).each do |user|
-      if user == answer.question.user
-        NewAnswerNotificationMailer.notification_for_owner(user, answer).try(:deliver_later) unless answer.user == user
+      if user.author_of?(answer.question)
+        unless user.author_of?(answer)
+          NewAnswerNotificationMailer.notification_for_owner(user, answer).try(:deliver_later)
+        end
       else
-        NewAnswerNotificationMailer.notification_for_user(user, answer).try(:deliver_later) unless answer.user == user
+        unless user.author_of?(answer)
+          NewAnswerNotificationMailer.notification_for_user(user, answer).try(:deliver_later)
+        end
       end
     end
   end
